b0a38508cf9ce99d9d5a8f78cfaf17c2a86d8782
Pierre <pierrejb.neveu@gmail.com>
2016-02-02 12:06:02 +0100

    Création  class Segment et Coords

---
+++ snake/snake/Coords.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    public struct Coords
+    {
+        public int x, y;
+
+        public Coords(int p1, int p2)
+        {
+            x = p1;
+            y = p2;
+
+        }
+    }
+}
--- snake/snake/Form1.Designer.cs
+++ snake/snake/Form1.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace snake
+﻿using System.Collections.Generic;
+namespace snake
 {
     partial class Form1
     {
@@ -6,7 +7,7 @@
         /// Variable nécessaire au concepteur.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-
+        
         /// <summary>
         /// Nettoyage des ressources utilisées.
         /// </summary>
@@ -28,20 +29,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+         
+            this.Grid_0x0 = new System.Windows.Forms.PictureBox();
+            
+            ((System.ComponentModel.ISupportInitialize)(this.Grid_0x0)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Snake
+            // 
+            
+
+                // 
+                // Grid_0x0
+                // 
+                this.Grid_0x0.Location = new System.Drawing.Point(0, 0);
+            this.Grid_0x0.Name = "Grid_0x0";
+            this.Grid_0x0.Size = new System.Drawing.Size(10, 10);
+            this.Grid_0x0.TabIndex = 0;
+            this.Grid_0x0.TabStop = false;
+
+           
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(758, 283);
+            this.ClientSize = new System.Drawing.Size(495, 318);
+            
             this.Name = "Form1";
             this.Text = "Form1";
+            
+            ((System.ComponentModel.ISupportInitialize)(this.Grid_0x0)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel Palne_Game;
+        private System.Windows.Forms.PictureBox Grid_0x0;
+        
+        
+        
     }
 }
 
